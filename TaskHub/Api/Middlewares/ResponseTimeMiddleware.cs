d1ab72ee1369c52fe5a34d99411451e34137d001
ffelx <161058483+ffelx@users.noreply.github.com>
2026-03-01 22:41:52 +0500

    Добавлены два middleware ResponseTimeMiddleware и StudentInfoMiddleware

---
+++ TaskHub/Api/Middlewares/ResponseTimeMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace Api.Middlewares
+{
+    public class ResponseTimeMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private const string HeaderTime = "X-Response-Time-Ms";
+
+        public ResponseTimeMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            context.Response.OnStarting(() =>
+            {
+                stopwatch.Stop();
+                context.Response.Headers[HeaderTime] = stopwatch.ElapsedMilliseconds.ToString();
+                return Task.CompletedTask;
+            });
+            await _next.Invoke(context);
+        }
+    }
+}
